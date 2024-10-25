b0a42951ee1adabbb8fc15faefa7b616b8e6d9da
Student-shivamsharma <165187758+Student-shivamsharma@users.noreply.github.com>
2024-10-25 13:09:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Main from './components/Main'
 import Skills from './components/Skills'
 import Experience from './components/Experience'
